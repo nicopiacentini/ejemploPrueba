0dc8aa1d0e673d8c65ef6f0e8a7b3db45f5a2834
nicopiacentini <npiacentini@frba.utn.edu.ar>
2024-06-03 17:21:05 -0300

    punto 2

--- samurai.hs
+++ samurai.hs
@@ -25,3 +25,19 @@ meditar personaje = modificarSalud personaje (1.5 * salud personaje)
 
 causarDanio :: Personaje -> Float -> Personaje
 causarDanio personaje danio = modificarSalud personaje (max 0 (salud personaje - danio))
+
+
+--2
+malvado :: Elemento -> Bool
+malvado elemento = "malvado" == tipo elemento
+esMalvado :: Personaje -> Bool
+esMalvado personaje = any malvado . elementos $ personaje
+
+danioQueProduce :: Personaje -> Elemento -> Float
+danioQueProduce personaje elemento = max 0 . (salud personaje -) . salud . ataque elemento $ personaje
+
+puedeMatarlo :: Personaje -> Personaje -> Bool
+puedeMatarlo asesinado asesino = any (==0) . map (danioQueProduce asesinado) . elementos $ asesino
+
+enemigosMortales :: Personaje -> [Personaje] -> [Personaje]
+enemigosMortales personaje enemigos = filter (puedeMatarlo personaje) enemigos
