e96ab5f0ebab36fcf2ef62ae55b4532e5d5098ab
nicopiacentini <npiacentini@frba.utn.edu.ar>
2024-06-04 15:48:46 -0300

    punto 4

--- uber.hs
+++ uber.hs
@@ -44,3 +44,8 @@ viajeDanielLucas = UnViaje 20042017 luquitas 150
 
 daniel :: Chofer
 daniel = UnChofer "Daniel" 23500 [viajeDanielLucas] (noViveEn "Olilvos")
+
+--4
+tomaElViaje :: Viaje -> Chofer -> Bool
+tomaElViaje viaje  = ($ viaje) . condicionDeViaje
+
